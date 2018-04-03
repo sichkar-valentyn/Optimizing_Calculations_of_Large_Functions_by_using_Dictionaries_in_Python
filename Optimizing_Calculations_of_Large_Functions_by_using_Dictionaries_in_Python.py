3ef1d9b028306d1127653bf2fb40431790d34ca1
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-03 12:34:37 +0300

    Update Optimizing_Calculations_of_Large_Functions_by_using_Dictionaries_in_Python.py

--- Optimizing_Calculations_of_Large_Functions_by_using_Dictionaries_in_Python.py
+++ Optimizing_Calculations_of_Large_Functions_by_using_Dictionaries_in_Python.py
@@ -1,4 +1,15 @@
-# Program are bein used for the large functions f() not to repeat the calculations but store them in the dictionary
+# File: Optimizing_Calculations_of_Large_Functions_by_using_Dictionaries_in_Python.py
+# Description: Optimizing Calculations of Large Functions by using Dictionaries in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Optimizing Calculations of Large Functions by using Dictionaries in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Optimizing_Calculations_of_Large_Functions_by_using_Dictionaries_in_Python (date of access: XX.XX.XXXX)
+
+# Program are being used for the large functions f() not to repeat the calculations but store them in the dictionary
 # It is called Optimizing of Calculations
 
 # Example of the function - it is needed to implememnt the task but in real can be with large calculations
